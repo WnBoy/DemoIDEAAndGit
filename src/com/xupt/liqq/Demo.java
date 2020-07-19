fd10319ca337340264cb73f3ca601150a2170f07
WnBoy <15029366160@163.com>
2020-07-19 22:25:50 +0800

    测试IDEA中的Git功能使用

---
+++ src/com/xupt/liqq/Demo.java
@@ -0,0 +1,13 @@
+package com.xupt.liqq;
+
+/**
+ * @author Wnlife
+ * @create 2020-07-19 22:04
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println("Nice");
+
+    }
+}
